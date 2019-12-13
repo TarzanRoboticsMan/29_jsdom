621d9b6582e2343e6dc85dd90cea1a98744391d9
Coby Sontag <TarzanRoboticsMan@users.noreply.github.com>
2019-12-13 09:29:12 -0500

    2nd

--- dommy.js
+++ dommy.js
@@ -1,9 +1,20 @@
 var bButton = document.getElementByID('b');
-bButton.addEventListener('click',addElement);
-
 var list = document.getElementsByTagName('li');
 
-var addElement() = function(){
+bButton.addEventListener('click',addElement);
+
+var current = function(){
+    return document.elementFromPoint(event.clientX,event.clientY)
+}
+
+for (var i=0; i,list.length; i++){
+    list[i].addEventListener('mousover',);
+    list[i].addEventListener('mousout',);
+    list[i].addEventListener('click',);
+}
+
+
+var addElement = function(){
     var item = document.createElement('li');
     var text = document.createTextNode('User added list element);
     item.appendChild(text);
